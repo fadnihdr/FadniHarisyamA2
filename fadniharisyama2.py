9a79244584c017121ba6ce5aafd3e5edf40bc76a
fadnihdr <fadnihdr@gmail.com>
2016-06-05 12:00:36 +0800

    - added additem - added functional saveitem - added exit_popup - added press_cancel

--- fadniharisyama2.py
+++ fadniharisyama2.py
@@ -6,7 +6,6 @@ from kivy.uix.button import Button
 class MyApp(App):
     def __init__(self, **kwargs):
         super(MyApp, self).__init__(**kwargs)
-        self.items_listed = open("items.csv", "r+")
 
 
     def build(self):
@@ -16,39 +15,64 @@ class MyApp(App):
         return self.root
 
     def create_buttons(self):
-        for each_line in self.items_listed:
+        file = open('items.csv', 'r')
+        for each_line in file:
             name, item_desc, cost, status = each_line.split(",")
             if "in" in status:
                 temp_button = Button(text=name, background_color=(0, 1, 0, 1))
             else:
                 temp_button = Button(text=name, background_color=(0.9, 0, 0.9, 1))
             self.root.ids.items_buttons.add_widget(temp_button)
+        file.close()
 
     def itemlist(self):
+        self.root.ids.items_buttons.clear_widgets()
         self.root.ids.label.text = 'Choose action from the left menu, then select items on the right'
-        self.root.ids.item_list.background_color = (0, 0.5, 0.8, 1)
-        self.root.ids.hire_item.background_color = (1, 1, 1, 1)
-        self.root.ids.return_item.background_color = (1, 1, 1, 1)
+        self.root.ids.list_button.background_color = (0, 0.5, 0.8, 1)
+        self.root.ids.hire_button.background_color = (1, 1, 1, 1)
+        self.root.ids.return_button.background_color = (1, 1, 1, 1)
         self.root.ids.confirm.background_color = (1, 1, 1, 1)
         self.root.ids.add_item.background_color = (1, 1, 1, 1)
         self.create_buttons()
 
     def itemreturn(self):
+        self.root.ids.items_buttons.clear_widgets()
         self.root.ids.label.text = 'Choose action from the left menu, then select items on the right'
-        self.root.ids.item_list.background_color = (1, 1, 1, 1)
-        self.root.ids.hire_item.background_color = (1, 1, 1, 1)
-        self.root.ids.return_item.background_color = (0, 0.5, 0.8, 1)
+        self.root.ids.list_button.background_color = (1, 1, 1, 1)
+        self.root.ids.hire_button.background_color = (1, 1, 1, 1)
+        self.root.ids.return_button.background_color = (0, 0.5, 0.8, 1)
         self.root.ids.confirm.background_color = (1, 1, 1, 1)
         self.root.ids.add_item.background_color = (1, 1, 1, 1)
         self.create_buttons()
 
     def itemhire(self):
+        self.root.ids.items_buttons.clear_widgets()
         self.root.ids.label.text = 'Choose action from the left menu, then select items on the right'
-        self.root.ids.item_list.background_color = (1, 1, 1, 1)
-        self.root.ids.hire_item.background_color = (0, 0.5, 0.8, 1)
-        self.root.ids.return_item.background_color = (1, 1, 1, 1)
+        self.root.ids.list_button.background_color = (1, 1, 1, 1)
+        self.root.ids.hire_button.background_color = (0, 0.5, 0.8, 1)
+        self.root.ids.return_button.background_color = (1, 1, 1, 1)
         self.root.ids.confirm.background_color = (1, 1, 1, 1)
         self.root.ids.add_item.background_color = (1, 1, 1, 1)
         self.create_buttons()
+
+    def additem(self):
+        self.root.ids.popup.open()
+
+    def saveitem(self, name, item_desc, cost):
+        add_item = "\n{},{},{},in".format(name, item_desc, cost)
+        with open("items.csv", "a") as file:
+            file.writelines(add_item)
+        print('Writing success')
+        self.exit_popup()
+        self.itemlist()
+
+    def exit_popup(self):
+        self.root.ids.popup.dismiss()
+
+    def press_cancel(self):
+        self.root.ids.iteminput.text = ""
+        self.root.ids.descinput.text = ""
+        self.root.ids.ppdinput.text = ""
+        self.root.ids.popup_label.text = "Enter details for new item"
 
 MyApp().run()
